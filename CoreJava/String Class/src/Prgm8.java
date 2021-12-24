ed7d6d7e0e1557cd4bdb0df4b902f4fbb94eb0af
VegetaCR7 <93119754+VegetaCR7@users.noreply.github.com>
2021-12-24 14:26:55 +0530

    Delete CoreJava directory

--- CoreJava/String Class/src/Prgm8.java
+++
@@ -1,9 +0,0 @@
-
-public class Prgm8 {
-
-	public static void main(String[] args) {
-		StringBuilder sbl=new StringBuilder("It is used to _ at the specified index position");
-		System.out.println(sbl.replace(14,15,"insert text"));
-	}
-
-}
